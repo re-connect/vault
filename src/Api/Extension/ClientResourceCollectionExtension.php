289c4e1133fca75b07e17a4cf828a9fb5042af87
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-02-09 17:43:55 +0100

    :sparkles: [API] Handle documents on filter collections

--- src/Api/Extension/ClientResourceCollectionExtension.php
+++ src/Api/Extension/ClientResourceCollectionExtension.php
@@ -3,37 +3,55 @@
 namespace App\Api\Extension;
 
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use App\Entity\Beneficiaire;
+use App\Entity\Document;
 use App\Security\HelperV2\Oauth2Helper;
 use Doctrine\ORM\QueryBuilder;
 
-final class ClientResourceCollectionExtension implements QueryCollectionExtensionInterface
+final class ClientResourceCollectionExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
+    private const HANDLED_CLASSES = [
+        Beneficiaire::class,
+        Document::class,
+    ];
+
     public function __construct(private readonly Oauth2Helper $oauth2Helper)
     {
     }
 
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, Operation $operation = null, array $context = []): void
+    {
+        $this->addClientExternalLinksFilter($queryBuilder, $resourceClass);
+    }
+
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
-        if (!$this->oauth2Helper->isClientGrantedAllPrivileges()) {
-            $this->addClientExternalLinksFilter($queryBuilder, $resourceClass);
-        }
+        $this->addClientExternalLinksFilter($queryBuilder, $resourceClass);
     }
 
     private function addClientExternalLinksFilter(QueryBuilder $queryBuilder, string $resourceClass): void
     {
-        if (Beneficiaire::class !== $resourceClass) {
+        $rootAliases = $queryBuilder->getRootAliases();
+        if (
+            !$this->oauth2Helper->isClientGrantedAllPrivileges()
+            || in_array($resourceClass, self::HANDLED_CLASSES)
+            || 0 === count($rootAliases)
+        ) {
             return;
         }
-        $rootAliases = $queryBuilder->getRootAliases();
-        if (0 === count($rootAliases)) {
-            return;
+        $rootAlias = $rootAliases[0];
+        $beneficiaryAlias = $rootAlias;
+
+        if (Document::class === $resourceClass) {
+            $beneficiaryAlias = 'beneficiary';
+            $queryBuilder->innerJoin(sprintf('%s.beneficiary', $rootAlias), $beneficiaryAlias);
         }
 
         $queryBuilder
-            ->innerJoin(sprintf('%s.externalLinks', $rootAliases[0]), 'externalLink')
+            ->innerJoin(sprintf('%s.externalLinks', $beneficiaryAlias), 'externalLink')
             ->innerJoin('externalLink.client', 'client')
             ->andWhere('client = :client')
             ->setParameter('client', $this->oauth2Helper->getClient());
