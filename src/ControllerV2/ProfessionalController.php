0b7dd9e50a396c54fd2c94111335ad8127a00038
AntoineAresu <you@example.com>
2023-03-10 12:44:04 +0100

    :sparkles: [BeneficiaryList] Implement pagination

--- src/ControllerV2/ProfessionalController.php
+++ src/ControllerV2/ProfessionalController.php
@@ -4,6 +4,7 @@ namespace App\ControllerV2;
 
 use App\FormV2\FilterBeneficiaryType;
 use App\Repository\BeneficiaireRepository;
+use App\ServiceV2\PaginatorService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,12 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProfessionalController extends AbstractController
 {
+    private const PAGINATION_RESULTS_LIMIT = 10;
+
     #[Route(path: '/professional/beneficiaries', name: 'list_beneficiaries', methods: ['GET'])]
     #[IsGranted('ROLE_MEMBRE')]
-    public function listBeneficiaries(BeneficiaireRepository $repository): Response
+    public function listBeneficiaries(Request $request, BeneficiaireRepository $repository, PaginatorService $paginator): Response
     {
         return $this->renderForm('v2/professional/beneficiaries.html.twig', [
-            'beneficiaries' => $repository->findByAuthorizedProfessional($this->getUser()->getSubject()),
+            'beneficiaries' => $paginator->create(
+                $repository->findByAuthorizedProfessional($this->getUser()->getSubject()),
+                $request->query->getInt('page', 1),
+                self::PAGINATION_RESULTS_LIMIT,
+            ),
             'form' => $this->createForm(FilterBeneficiaryType::class, null, [
                 'action' => $this->generateUrl('search_beneficiaries'),
                 'attr' => ['data-controller' => 'ajax-list-filter'],
@@ -32,21 +39,25 @@ class ProfessionalController extends AbstractController
         condition: 'request.isXmlHttpRequest()',
     )]
     #[IsGranted('ROLE_MEMBRE')]
-    public function searchBeneficiaries(Request $request, BeneficiaireRepository $repository): Response
+    public function searchBeneficiaries(Request $request, BeneficiaireRepository $repository, PaginatorService $paginator): Response
     {
-        $formData = $request->request->all()['filter_beneficiary'];
+        $form = $this->createForm(FilterBeneficiaryType::class, null, [
+            'action' => $this->generateUrl('search_beneficiaries'),
+            'attr' => ['data-controller' => 'ajax-list-filter'],
+        ])->handleRequest($request);
 
         return new JsonResponse([
             'html' => $this->renderForm('v2/professional/_beneficiaries_list.html.twig', [
-                'beneficiaries' => $repository->filterByAuthorizedProfessional(
-                    $this->getUser()->getSubject(),
-                    $formData['search'],
-                    $formData['relay'] ?? null,
+                'beneficiaries' => $paginator->create(
+                    $repository->filterByAuthorizedProfessional(
+                        $this->getUser()->getSubject(),
+                        $form->get('search')->getData(),
+                        $form->get('relay')->getData(),
+                    ),
+                    $request->query->getInt('page', 1),
+                    self::PAGINATION_RESULTS_LIMIT,
                 ),
-                'form' => $this->createForm(FilterBeneficiaryType::class, null, [
-                    'action' => $this->generateUrl('search_beneficiaries'),
-                    'attr' => ['data-controller' => 'ajax-list-filter'],
-                ]),
+                'form' => $form,
             ])->getContent(),
         ]);
     }
