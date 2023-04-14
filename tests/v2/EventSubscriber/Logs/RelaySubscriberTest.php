6d94182f6a59c54b9f72658f6fffed239d85f4f5
AntoineAresu <aresu.antoine@gmail.com>
2023-04-14 13:30:35 +0200

    :white_check_mark: [ActivityLogs] Add tests

---
+++ tests/v2/EventSubscriber/Logs/RelaySubscriberTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Tests\v2\EventSubscriber\Logs;
+
+use App\Entity\Centre;
+use App\EventSubscriber\Logs\RelaySubscriber;
+use App\Tests\Factory\RelayFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Zenstruck\Foundry\Test\Factories;
+
+class RelaySubscriberTest extends AbstractLogActivitySubscriberTest implements TestLogActivitySubscriberInterface
+{
+    use Factories;
+
+    private const LOG_FILE_NAME = 'relay.log';
+    private ?RelaySubscriber $relaySubscriber;
+    private ?EntityManagerInterface $em;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->relaySubscriber = $this->getContainer()->get(RelaySubscriber::class);
+        $this->em = $this->getContainer()->get(EntityManagerInterface::class);
+    }
+
+    public function testEventSubscriptions(): void
+    {
+        $this->assertEventSubscriptions($this->relaySubscriber->getSubscribedEvents());
+    }
+
+    public function testPostPersist(): void
+    {
+        $relay = (new Centre())
+            ->setNom('test');
+
+        $this->em->persist($relay);
+        $this->em->flush();
+
+        $this->assertLastLog(self::LOG_FILE_NAME, ['Relay created', ...$this->getLogContent($relay)]);
+    }
+
+    public function testPreUpdate(): void
+    {
+        $relay = RelayFactory::random()->object();
+        $relay->setNom('test');
+        $this->em->flush();
+
+        $this->assertLastLog(self::LOG_FILE_NAME, ['Relay updated', ...$this->getLogContent($relay)]);
+    }
+
+    public function testPreRemove(): void
+    {
+        $relay = RelayFactory::random()->object();
+        $this->em->remove($relay);
+        $logContent = $this->getLogContent($relay);
+        $this->em->flush();
+
+        $this->assertLastLog(self::LOG_FILE_NAME, ['Relay removed', ...$logContent]);
+    }
+
+    private function getLogContent(Centre $relay): array
+    {
+        return [
+            'user_id' => $relay->getId(),
+            'by_user_id' => $this->loggedUser?->getId(),
+        ];
+    }
+}
---
+++ tests/v2/EventSubscriber/Logs/TestLogActivitySubscriberInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\v2\EventSubscriber\Logs;
+
+interface TestLogActivitySubscriberInterface
+{
+    public function testEventSubscriptions(): void;
+    public function testPostPersist(): void;
+    public function testPreUpdate(): void;
+    public function testPreRemove(): void;
+}
