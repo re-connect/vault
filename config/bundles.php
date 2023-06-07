1912cf4234964a066e7c8a773c5f14b0bc300354
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-06-07 09:52:13 +0200

    :arrow_up: [Recipes] Upgrade

--- config/bundles.php
+++ config/bundles.php
@@ -26,7 +26,7 @@ return [
     Sonata\IntlBundle\SonataIntlBundle::class => ['all' => true],
     Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
