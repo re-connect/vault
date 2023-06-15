55737f3375ba1578914d27e62570e026158cb1b7
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-06-15 13:45:22 +0200

    :sparkles: [Create Member] Add Search route

--- src/ControllerV2/ProController.php
+++ src/ControllerV2/ProController.php
@@ -6,6 +6,7 @@ use App\FormV2\Search\SearchFormModel;
 use App\FormV2\Search\SearchType;
 use App\Security\VoterV2\ProVoter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +15,28 @@ class ProController extends AbstractController
 {
     #[Route(path: '/create/home', name: 'create_pro_home', methods: ['GET'])]
     #[IsGranted(ProVoter::MANAGE)]
-    public function listBeneficiaries(): Response
+    public function createProHome(): Response
     {
-        $form = $this->createForm(SearchType::class, new SearchFormModel());
+        $form = $this->createForm(SearchType::class, new SearchFormModel(), [
+            'action' => $this->generateUrl('search_pro'),
+        ]);
 
         return $this->render('v2/pro/create_index.html.twig', ['form' => $form]);
+    }
+
+    #[Route(path: '/search', name: 'search_pro', methods: ['GET', 'POST'])]
+    #[IsGranted(ProVoter::MANAGE)]
+    public function searchPros(Request $request): Response
+    {
+        $search = new SearchFormModel($request->query->getAlpha('q'));
+        $form = $this->createForm(SearchType::class, $search, [
+            'action' => $this->generateUrl('search_pro'),
+        ])->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('search_pro', ['q' => $search->getSearch()]);
+        }
+
+        return $this->render('v2/pro/search.html.twig', ['form' => $form]);
     }
 }
