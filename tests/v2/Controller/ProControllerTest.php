9cecebbd77db52fc1979135e5c4b8de09ac6357e
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-06-15 13:52:44 +0200

    :white_check_mark: [Search pro] Add tests

--- tests/v2/Controller/ProControllerTest.php
+++ tests/v2/Controller/ProControllerTest.php
@@ -7,7 +7,8 @@ use App\DataFixtures\v2\MemberFixture;
 
 class ProControllerTest extends AbstractControllerTest implements TestRouteInterface
 {
-    private const URL = '/pro/create/home';
+    private const HOME_URL = '/pro/create/home';
+    private const SEARCH_URL = '/pro/search';
 
     protected function setUp(): void
     {
@@ -19,8 +20,8 @@ class ProControllerTest extends AbstractControllerTest implements TestRouteInter
     public function testRoute(
         string $url,
         int $expectedStatusCode,
-        ?string $userMail = null,
-        ?string $expectedRedirect = null,
+        string $userMail = null,
+        string $expectedRedirect = null,
         string $method = 'GET',
         bool $isXmlHttpRequest = false,
         array $body = [],
@@ -30,9 +31,33 @@ class ProControllerTest extends AbstractControllerTest implements TestRouteInter
 
     public function provideTestRoute(): ?\Generator
     {
-        yield 'Should redirect to login when not authenticated' => [self::URL, 302, null, '/login'];
-        yield 'Should return 200 status code when authenticated as member' => [self::URL, 200, MemberFixture::MEMBER_MAIL_WITH_RELAYS];
-        yield 'Should return 403 status code when authenticated as member without permissions' => [self::URL, 403, BeneficiaryFixture::BENEFICIARY_MAIL];
-        yield 'Should return 403 status code when authenticated as beneficiary' => [self::URL, 403, MemberFixture::MEMBER_MAIL];
+        yield 'Should redirect to login when not authenticated' => [self::HOME_URL, 302, null, '/login'];
+        yield 'Should return 200 status code when authenticated as member' => [self::HOME_URL, 200, MemberFixture::MEMBER_MAIL_WITH_RELAYS];
+        yield 'Should return 403 status code when authenticated as member without permissions' => [self::HOME_URL, 403, BeneficiaryFixture::BENEFICIARY_MAIL];
+        yield 'Should return 403 status code when authenticated as beneficiary' => [self::HOME_URL, 403, MemberFixture::MEMBER_MAIL];
+    }
+
+    /**  @dataProvider provideTestFormIsValid */
+    public function testFormIsValid(string $url, string $formSubmit, array $values, ?string $email, ?string $redirectUrl): void
+    {
+        $this->assertFormIsValid($url, $formSubmit, $values, $email, $redirectUrl);
+    }
+
+    public function provideTestFormIsValid(): ?\Generator
+    {
+        yield 'Home submit search should redirect to search and set the url param' => [
+            self::HOME_URL,
+            'search',
+            ['search[search]' => 'gollum'],
+            MemberFixture::MEMBER_MAIL_WITH_RELAYS,
+            sprintf('%s?q=%s', self::SEARCH_URL, 'gollum'),
+        ];
+        yield 'Search submit search should redirect to search' => [
+            self::SEARCH_URL,
+            'search',
+            ['search[search]' => 'Gandalf'],
+            MemberFixture::MEMBER_MAIL_WITH_RELAYS,
+            sprintf('%s?q=%s', self::SEARCH_URL, 'Gandalf'),
+        ];
     }
 }
