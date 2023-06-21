f2f20f46ca93d77fd8a07d2a53931d109f9fad2c
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2023-06-21 18:52:04 +0200

    :sparkles: [User] Add route to create pro

--- src/ControllerV2/ProController.php
+++ src/ControllerV2/ProController.php
@@ -2,11 +2,16 @@
 
 namespace App\ControllerV2;
 
+use App\Entity\Membre;
+use App\Entity\User;
 use App\FormV2\Search\SearchFormModel;
 use App\FormV2\Search\SearchType;
+use App\FormV2\UserCreation\CreateUserType;
+use App\ManagerV2\UserManager;
 use App\Repository\MembreRepository;
 use App\Security\VoterV2\ProVoter;
 use App\ServiceV2\PaginatorService;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +29,24 @@ class ProController extends AbstractController
         ]);
 
         return $this->render('v2/pro/create_index.html.twig', ['form' => $form]);
+    }
+
+    #[Route(path: '/create', name: 'create_pro', methods: ['GET', 'POST'])]
+    #[IsGranted(ProVoter::MANAGE)]
+    public function createPro(Request $request, EntityManagerInterface $em, UserManager $manager): Response
+    {
+        $user = (new User())->setSubjectMembre(new Membre());
+        $form = $this->createForm(CreateUserType::class, $user, [
+            'action' => $this->generateUrl('create_pro'),
+        ])->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->updatePasswordWithPlain($user);
+
+            return $this->redirectToRoute('invite_user', ['id' => $user->getId()]);
+        }
+
+        return $this->render('v2/pro/create.html.twig', ['form' => $form]);
     }
 
     #[Route(path: '/search', name: 'search_pro', methods: ['GET', 'POST'])]
