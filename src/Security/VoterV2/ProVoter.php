d85ab4110c04f2f07ba04ee712d0cb4e9b43f41b
AntoineAresu <aresu.antoine@gmail.com>
2023-06-21 18:15:47 +0200

    :sparkles: [UserVoter] Refacto in order to check update attribute on user subject

--- src/Security/VoterV2/ProVoter.php
+++ src/Security/VoterV2/ProVoter.php
@@ -2,18 +2,26 @@
 
 namespace App\Security\VoterV2;
 
+use App\Entity\Membre;
 use App\Entity\MembreCentre;
 use App\Entity\User;
+use App\Security\HelperV2\UserHelper;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class ProVoter extends Voter
 {
     public const MANAGE = 'MANAGE_PRO';
+    public const UPDATE = 'UPDATE';
+
+    public function __construct(private readonly UserHelper $helper)
+    {
+    }
 
     protected function supports(string $attribute, $subject): bool
     {
-        return self::MANAGE == $attribute;
+        return in_array($attribute, [self::MANAGE, self::UPDATE])
+            && (!$subject || $subject instanceof Membre);
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
@@ -23,6 +31,10 @@ class ProVoter extends Voter
             return false;
         }
 
-        return $user->hasDroit(MembreCentre::TYPEDROIT_GESTION_MEMBRES);
+        return match ($attribute) {
+            self::MANAGE => $user->hasDroit(MembreCentre::TYPEDROIT_GESTION_MEMBRES),
+            self::UPDATE => $this->helper->canUpdateProfessional($user, $subject),
+            default => false,
+        };
     }
 }
