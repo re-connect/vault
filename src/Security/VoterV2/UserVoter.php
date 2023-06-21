2da3cec72b46312576b6323da84f4b07b13189c7
AntoineAresu <aresu.antoine@gmail.com>
2023-06-21 17:35:41 +0200

    :recycle: [UserVoter] Refacto dummy attributes

--- src/Security/VoterV2/UserVoter.php
+++ src/Security/VoterV2/UserVoter.php
@@ -8,15 +8,14 @@ use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class UserVoter extends Voter
 {
-    public const SELF_EDIT = 'SELF_EDIT';
-    public const DELETE_BENEFICIARY = 'DELETE_BENEFICIARY';
+    public const SELF_DELETE = 'SELF_DELETE';
 
     /**
      * @param object $subject
      */
     protected function supports(string $attribute, $subject): bool
     {
-        return in_array($attribute, [self::SELF_EDIT, self::DELETE_BENEFICIARY])
+        return in_array($attribute, [self::SELF_DELETE])
             && $subject instanceof User;
     }
 
@@ -31,19 +30,13 @@ class UserVoter extends Voter
         }
 
         return match ($attribute) {
-            self::SELF_EDIT => $this->canSelfEdit($user, $subject),
-            self::DELETE_BENEFICIARY => $this->canDeleteBeneficiary($user, $subject),
+            self::SELF_DELETE => $this->canSelfDelete($user, $subject),
             default => false,
         };
     }
 
-    private function canSelfEdit(User $user, User $subject): bool
+    private function canSelfDelete(User $user, User $subject): bool
     {
-        return $user->isValidUser() && $user === $subject;
-    }
-
-    private function canDeleteBeneficiary(User $user, User $subject): bool
-    {
-        return $this->canSelfEdit($user, $subject) && $user->isBeneficiaire();
+        return $user === $subject && $user->isBeneficiaire();
     }
 }
