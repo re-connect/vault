8302f513df169ca108ebf4de533ab19ea452c25c
Nils Lapôtre <nils.lapotre@reconnect.fr>
2023-07-11 10:01:35 +0200

    :recycle: [HomeController] Use request stack instead of passing route as argument

--- src/ControllerV2/HomeController.php
+++ src/ControllerV2/HomeController.php
@@ -3,16 +3,17 @@
 namespace App\ControllerV2;
 
 use App\Entity\Beneficiaire;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
     #[IsGranted('ROLE_USER')]
-    public function renderBeneficiaryNav(?Beneficiaire $beneficiary, ?string $currentRoute = ''): Response
+    public function renderBeneficiaryNav(?Beneficiaire $beneficiary, RequestStack $stack): Response
     {
         if ($subjectBeneficiary = $this->getUser()?->getSubjectBeneficiaire()) {
-            return $this->render('v2/vault/nav/beneficiary/_beneficiary_nav.html.twig', ['beneficiary' => $subjectBeneficiary, 'route' => $currentRoute]);
+            return $this->render('v2/vault/nav/beneficiary/_beneficiary_nav.html.twig', ['beneficiary' => $subjectBeneficiary, 'route' => $stack->getMainRequest()->attributes->get('_route')]);
         } elseif ($beneficiary) {
             return $this->render('v2/vault/nav/pro/_pro_nav.html.twig', ['beneficiary' => $beneficiary]);
         }
