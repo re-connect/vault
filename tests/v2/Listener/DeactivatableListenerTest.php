944140c2a3d44f4f7e08a1be7e7b9b70a5e01725
AntoineAresu <aresu.antoine@gmail.com>
2023-09-25 17:00:51 +0200

    :white_check_mark: [DeactivatableListenerTest] Add tests

--- tests/v2/Listener/DeactivatableListenerTest.php
+++ tests/v2/Listener/DeactivatableListenerTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests\v2\Listener;
 
 use App\DataFixtures\v2\MemberFixture;
+use App\Entity\MembreCentre;
 use App\Entity\User;
+use App\Tests\Factory\RelayFactory;
 use App\Tests\Factory\UserFactory;
 use App\Tests\v2\AuthenticatedKernelTestCase;
 use Doctrine\ORM\EntityManagerInterface;
@@ -75,5 +77,37 @@ class DeactivatableListenerTest extends AuthenticatedKernelTestCase
 
         $this->assertEquals($randomUser, $user->getDisabledBy());
         $this->assertEquals($today->format('d/m/Y'), $user->getDisabledAt()?->format('d/m/Y'));
+    }
+
+    public function testShouldEnableOnNewUserRelay(): void
+    {
+        $randomUser = UserFactory::findOrCreate(['email' => MemberFixture::MEMBER_DISABLED])->object();
+        $randomRelay = RelayFactory::createOne()->object();
+        self::assertFalse($randomUser->isEnabled());
+
+        $userRelay = (new MembreCentre())->setCentre($randomRelay)->setUser($randomUser);
+        $this->em->persist($userRelay);
+        $this->em->flush();
+
+        self::assertTrue($randomUser->isEnabled());
+    }
+
+    /**  @dataProvider provideTestDisableWhenRemovingRelay */
+    public function testDisableWhenRemovingRelay(string $userMail, int $relayCount, bool $shouldBeEnabled): void
+    {
+        $randomUser = UserFactory::findOrCreate(['email' => $userMail])->object();
+        self::assertTrue($randomUser->isEnabled());
+        self::assertCount($relayCount, $randomUser->getCentres());
+
+        $this->em->remove($randomUser->getUserRelays()->first());
+        $this->em->flush();
+
+        self::assertEquals($shouldBeEnabled, $randomUser->isEnabled());
+    }
+
+    public function provideTestDisableWhenRemovingRelay(): \Generator
+    {
+        yield 'Should disable pro user when removing unique relay' => [MemberFixture::MEMBER_MAIL_WITH_UNIQUE_RELAY_SHARED_WITH_BENEFICIARIES, 1, false];
+        yield 'Should not disable pro user when removing one relay' => [MemberFixture::MEMBER_MAIL, 2, true];
     }
 }
