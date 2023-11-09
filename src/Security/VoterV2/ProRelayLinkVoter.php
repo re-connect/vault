711b1f5cdea922f2f491e10c707c091d1ec7dbbe
AntoineAresu <aresu.antoine@gmail.com>
2023-11-09 12:15:35 +0100

    :lock: [TogglePermission] Allow to toggle permission if I own it

---
+++ src/Security/VoterV2/ProRelayLinkVoter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Security\VoterV2;
+
+use App\Entity\MembreCentre;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class ProRelayLinkVoter extends Voter
+{
+    public function __construct(private readonly AuthorizationCheckerInterface $authorizationChecker)
+    {
+    }
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, MembreCentre::PERMISSIONS) && $subject instanceof MembreCentre;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        $relay = $subject->getCentre();
+
+        if (!$this->authorizationChecker->isGranted('UPDATE', $subject->getMembre())
+            || !$this->authorizationChecker->isGranted('MANAGE_PRO', $relay)) {
+            return false;
+        }
+
+        return $user->hasPermissionOnRelay($relay, $attribute);
+    }
+}
