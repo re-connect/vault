6027faa0b20e40cf09a1e79020c541195ce52e54
AntoineAresu <aresu.antoine@gmail.com>
2023-11-15 16:59:40 +0100

    :sparkles: [CheckBeneficiaryOnRosalie] Improve flash messages

--- src/ControllerV2/RosalieInteroperabilityController.php
+++ src/ControllerV2/RosalieInteroperabilityController.php
@@ -14,13 +14,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RosalieInteroperabilityController extends AbstractController
 {
+    public function __construct(
+        private readonly RosalieService $service,
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
     #[IsGranted('ROLE_MEMBRE')]
     #[Route('/beneficiaries/{id}/add-si-siao-number', name: 'add_si_siao_number')]
-    public function addSiSiaoNumber(Request $request, Beneficiaire $beneficiary, RosalieService $service, TranslatorInterface $translator, EntityManagerInterface $em): Response
+    public function addSiSiaoNumber(Request $request, Beneficiaire $beneficiary): Response
     {
         $beneficiaryCreationProcess = $beneficiary->getCreationProcess();
 
@@ -28,17 +33,17 @@ class RosalieInteroperabilityController extends AbstractController
             ? $this->redirectToRoute('create_beneficiary', ['id' => $beneficiaryCreationProcess->getId(), 'step' => $beneficiaryCreationProcess->getLastReachedStep()])
             : $this->redirectToRoute('list_beneficiaries');
 
-        return $this->processSiSiaoNumberForm($request, $beneficiary, $service, $translator, $em, $redirection);
+        return $this->processSiSiaoNumberForm($request, $beneficiary, $redirection);
     }
 
     #[IsGranted('ROLE_MEMBRE')]
     #[Route('/beneficiary/{id}/affiliate/add-si-siao-number', name: 'affiliate_beneficiary_add_si_siao_number')]
-    public function affiliationAddSiSiaoNumber(Request $request, Beneficiaire $beneficiary, RosalieService $service, TranslatorInterface $translator, EntityManagerInterface $em): Response
+    public function affiliationAddSiSiaoNumber(Request $request, Beneficiaire $beneficiary): Response
     {
-        return $this->processSiSiaoNumberForm($request, $beneficiary, $service, $translator, $em, $this->redirectToRoute('affiliate_beneficiary_relays', ['id' => $beneficiary->getId()]));
+        return $this->processSiSiaoNumberForm($request, $beneficiary, $this->redirectToRoute('affiliate_beneficiary_relays', ['id' => $beneficiary->getId()]));
     }
 
-    private function processSiSiaoNumberForm(Request $request, Beneficiaire $beneficiary, RosalieService $service, TranslatorInterface $translator, EntityManagerInterface $em, RedirectResponse $redirection): Response
+    private function processSiSiaoNumberForm(Request $request, Beneficiaire $beneficiary, RedirectResponse $redirection): Response
     {
         $form = $this->createFormBuilder()
             ->add('number', TextType::class, ['label' => 'si_siao_number', 'data' => $beneficiary->getSiSiaoNumber()])
@@ -46,15 +51,18 @@ class RosalieInteroperabilityController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $beneficiary->setSiSiaoNumber($form->get('number')->getData());
-            if ($service->beneficiaryExistsOnRosalie($beneficiary)) {
-                $service->linkBeneficiaryToRosalie($beneficiary);
-                $this->addFlash('success', $translator->trans('si_siao_number_found_rosalie'));
+            $this->em->flush();
+
+            $beneficiaryCheck = $this->service->checkBeneficiaryOnRosalie($beneficiary);
+
+            if ($beneficiaryCheck->beneficiaryIsFound()) {
+                $this->service->linkBeneficiaryToRosalie($beneficiaryCheck->getBeneficiary());
+                $this->addFlash('success', 'si_siao_number_found_rosalie');
+            } elseif (str_contains($this->getUser()?->getEmail() ?? '', '@samusocial-75')) {
+                $this->addFlash('error', $beneficiaryCheck->getSamuSocialErrorMessage());
             }
-            $em->flush();
 
             return $redirection;
-        } elseif (str_contains($this->getUser()?->getEmail() ?? '', '@samusocial-75')) {
-            $this->addFlash('error', $translator->trans('si_siao_number_not_found_rosalie'));
         }
 
         return $this->render('v2/rosalie/add_si_siao_number.html.twig', [
