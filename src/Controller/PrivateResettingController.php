257873befdac83ec92043659d076271691300503
Antoine Aresu <aresu.antoine@gmail.com>
2023-12-03 21:31:13 +0100

    :bug: [Gdpr/updatePassword] Fix test, v2 route using v1 template

--- src/Controller/PrivateResettingController.php
+++ src/Controller/PrivateResettingController.php
@@ -3,19 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\Entity\PasswordResetSecretQuestion;
-use App\Form\Entity\PasswordResetSMS;
-use App\Form\Type\PasswordResetSecretQuestionType;
-use App\Form\Type\PasswordResetSMSType;
-use App\Manager\UserManager;
-use App\ManagerV2\UserManager as UserManagerV2;
 use App\RepositoryV2\ResetPasswordRequestRepository;
-use App\ServiceV2\ResettingService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
