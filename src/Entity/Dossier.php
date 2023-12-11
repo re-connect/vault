a7a7f41a7f39301d6bfa75bdc2232b4001936282
AntoineAresu <aresu.antoine@gmail.com>
2023-12-11 10:45:54 +0100

    :recycle: [Cs] Fix

--- src/Entity/Dossier.php
+++ src/Entity/Dossier.php
@@ -14,9 +14,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\JoinColumn;
-use Doctrine\ORM\Mapping\ManyToOne;
-use Doctrine\ORM\Mapping\OneToMany;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
