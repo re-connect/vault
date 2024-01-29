20dda1d37195feadd3642098d33d1e149d8ca2f6
NilsRec <nils.lapotre@reconnect.fr>
2024-01-29 17:33:38 +0100

    :card_file_box: [User] Add personal data request date

---
+++ src/Migrations/Version20240129152854.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240129152854 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user ADD personalAccountDataRequestedAt DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user DROP personalAccountDataRequestedAt');
+    }
+}
