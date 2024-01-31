f8599a752d919fca0da9d4e4f8754fc44ab84c68
AntoineAresu <aresu.antoine@gmail.com>
2024-01-31 12:29:47 +0100

    :recycle: [BeneficiaryConsultation] Refacto using custom event

---
+++ src/EventSubscriber/BeneficiaryConsultationSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\EventV2\BeneficiaryConsultationEvent;
+use App\ManagerV2\MemberBeneficiaryManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+readonly class BeneficiaryConsultationSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private MemberBeneficiaryManager $memberBeneficiaryManager,
+    ) {
+    }
+
+    public function recordBeneficiaryConsultation(BeneficiaryConsultationEvent $event): void
+    {
+        $this->memberBeneficiaryManager->recordBeneficiaryConsultation($event->getBeneficiary());
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeneficiaryConsultationEvent::class => 'recordBeneficiaryConsultation',
+        ];
+    }
+}
