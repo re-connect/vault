b92612d13e078e58efab2660791dbfefcf00c77a
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-02-02 14:40:59 +0100

    :recycle: [Oauth2TokenIssued] Refacto subscriber

--- src/EventSubscriber/Api/Oauth2TokenIssuedSubscriber.php
+++ src/EventSubscriber/Api/Oauth2TokenIssuedSubscriber.php
@@ -7,41 +7,33 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use League\OAuth2\Server\RequestAccessTokenEvent;
 use League\OAuth2\Server\RequestEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class Oauth2TokenIssuedSubscriber implements EventSubscriberInterface
+#[AsEventListener(RequestEvent::ACCESS_TOKEN_ISSUED, 'onAccessTokenIssued')]
+readonly class Oauth2TokenIssuedSubscriber
 {
-    private EventDispatcherInterface $dispatcher;
-    private UserRepository $repository;
-    private EntityManagerInterface $em;
-
-    public function __construct(EventDispatcherInterface $dispatcher, UserRepository $repository, EntityManagerInterface $em)
-    {
-        $this->dispatcher = $dispatcher;
-        $this->repository = $repository;
-        $this->em = $em;
+    public function __construct(
+        private EventDispatcherInterface $dispatcher,
+        private UserRepository $repository,
+        private EntityManagerInterface $em,
+    ) {
     }
 
     public function onAccessTokenIssued(RequestAccessTokenEvent $event): void
     {
         $userId = (string) $event->getAccessToken()->getUserIdentifier();
-        if ($userId) {
-            $user = $this->repository->loadUserByIdentifier($userId);
-            if ($user) {
-                if (!$user->hasLoginToday()) {
-                    $user->setDerniereConnexionAt(new \DateTime());
-                    $this->em->flush();
-                }
-                $this->dispatcher->dispatch(new UserEvent($user, !$user->hasLoginToday()));
-            }
+        if (!$userId) {
+            return;
         }
-    }
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            RequestEvent::ACCESS_TOKEN_ISSUED => 'onAccessTokenIssued',
-        ];
+        $user = $this->repository->loadUserByIdentifier($userId);
+        if (!$user) {
+            return;
+        }
+        if (!$user->hasLoginToday()) {
+            $user->setDerniereConnexionAt(new \DateTime());
+            $this->em->flush();
+        }
+        $this->dispatcher->dispatch(new UserEvent($user, !$user->hasLoginToday()));
     }
 }
