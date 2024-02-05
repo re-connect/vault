90cedaf784e53f250065965e3b4cb5ab62912531
Antoine Aresu <aresu.antoine@gmail.com>
2024-02-05 17:37:59 +0100

    :sparkles: [ImprovePassword] Add route

--- src/ControllerV2/GdprController.php
+++ src/ControllerV2/GdprController.php
@@ -2,9 +2,11 @@
 
 namespace App\ControllerV2;
 
+use App\Entity\User;
 use App\FormV2\ChangePasswordFormType;
 use App\ManagerV2\UserManager;
 use App\ServiceV2\GdprService;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +17,7 @@ class GdprController extends AbstractController
     public function updatePassword(Request $request, UserManager $userManager, GdprService $gdprService): Response
     {
         $user = $this->getUser();
-        $form = $this->createForm(ChangePasswordFormType::class, null, ['isBeneficiaire' => $user?->isBeneficiaire()])->handleRequest($request);
+        $form = $this->createPasswordForm($user)->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid() && $user) {
             $newPassword = $form->get('plainPassword')->getData();
             if (!$userManager->isPasswordValid($user, $newPassword)) {
@@ -29,6 +31,32 @@ class GdprController extends AbstractController
             $gdprService->showPasswordRenewalFlash();
         }
 
+        return $this->renderPasswordPage($form);
+    }
+
+    #[Route(path: '/improve-password', name: 'improve_password', methods: ['GET', 'POST'])]
+    public function improvePassword(Request $request, UserManager $userManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createPasswordForm($user)->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setHasPasswordWithLatestPolicy(true);
+            $userManager->updatePassword($user, $form->get('plainPassword')->getData());
+            $this->addFlash('success', 'password_updated_successfully');
+
+            return $this->redirectToRoute('redirect_user');
+        }
+
+        return $this->renderPasswordPage($form);
+    }
+
+    private function createPasswordForm(?User $user): FormInterface
+    {
+        return $this->createForm(ChangePasswordFormType::class, null, ['isBeneficiaire' => $user?->isBeneficiaire()]);
+    }
+
+    private function renderPasswordPage(FormInterface $form): Response
+    {
         return $this->render('v2/user/update_password_form.html.twig', [
             'passwordForm' => $form,
         ]);
