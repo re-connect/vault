2e7c6d0be4a2ef3c59234a876ab5813d148e6291
Antoine Aresu <aresu.antoine@gmail.com>
2024-02-05 15:36:53 +0100

    :white_check_mark: [ImprovePassword] Add tests

---
+++ tests/v2/EventSubscriber/StrongPasswordSubscriberTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\v2\EventSubscriber;
+
+use App\DataFixtures\v2\BeneficiaryFixture;
+use App\DataFixtures\v2\MemberFixture;
+use App\EventSubscriber\StrongPasswordSubscriber;
+use App\Tests\Factory\UserFactory;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Zenstruck\Foundry\Test\Factories;
+
+class StrongPasswordSubscriberTest extends WebTestCase
+{
+    use Factories;
+    private const RANDOM_URLS = [];
+
+    public function testEventSubscription(): void
+    {
+        $this->assertArrayHasKey(RequestEvent::class, StrongPasswordSubscriber::getSubscribedEvents());
+    }
+
+    /**
+     * @dataProvider provideTestRedirectsToImprovePasswordPage
+     */
+    public function testRedirectsToImprovePasswordPage(string $email): void
+    {
+        self::ensureKernelShutdown();
+        $user = UserFactory::findByEmail($email)->object();
+
+        self::assertNull($user->hasUpdatedPasswordWithLastPolicy());
+
+        $client = static::createClient();
+        $client->loginUser($user);
+
+        // Redirect after login
+        self::assertResponseRedirects('/improve-password');
+
+        // Redirects for any other page
+        foreach (self::RANDOM_URLS as $url) {
+            $client->request('GET', $url);
+            self::assertResponseRedirects('/improve-password');
+        }
+    }
+
+    public function provideTestRedirectsToImprovePasswordPage(): \Generator
+    {
+        yield 'Should redirect for beneficiary user' => [BeneficiaryFixture::BENEFICIARY_MAIL];
+        yield 'Should redirect for beneficiary user' => [MemberFixture::MEMBER_MAIL];
+    }
+}
