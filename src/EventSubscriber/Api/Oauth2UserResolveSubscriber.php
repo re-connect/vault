006c013bda21c2ad84e6b9e6317a59bf050e2ab5
NilsRec <nils.lapotre@reconnect.fr>
2024-02-05 15:51:15 +0100

    :lock: [OAuth2 MFA] Can access using client_credentials

--- src/EventSubscriber/Api/Oauth2UserResolveSubscriber.php
+++ src/EventSubscriber/Api/Oauth2UserResolveSubscriber.php
@@ -5,11 +5,7 @@ namespace App\EventSubscriber\Api;
 use App\Repository\UserRepository;
 use League\Bundle\OAuth2ServerBundle\Event\UserResolveEvent;
 use League\Bundle\OAuth2ServerBundle\OAuth2Events;
-use League\Bundle\OAuth2ServerBundle\Security\User\NullUser;
-use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Email\Generator\CodeGeneratorInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\User\LegacyPasswordAuthenticatedUserInterface;
 
@@ -19,8 +15,6 @@ readonly class Oauth2UserResolveSubscriber
     public function __construct(
         private UserPasswordHasherInterface $hasher,
         private UserRepository $repository,
-        private CodeGeneratorInterface $codeGenerator,
-        private RequestStack $requestStack,
     ) {
     }
 
@@ -29,20 +23,6 @@ readonly class Oauth2UserResolveSubscriber
         $user = $this->repository->loadUserByIdentifier($event->getUsername());
         $isUserPasswordInvalid = $user instanceof LegacyPasswordAuthenticatedUserInterface && !$this->hasher->isPasswordValid($user, $event->getPassword());
         $event->setUser($isUserPasswordInvalid ? null : $user);
-
-        if (!$user->isMfaEnabled()) {
-            return $event;
-        }
-
-        $currentRequest = $this->requestStack->getCurrentRequest();
-        $sentAuthCode = $currentRequest->request->get('_auth_code');
-
-        if (!$sentAuthCode) {
-            $event->setUser(new NullUser());
-            $this->codeGenerator->generateAndSend($user);
-        } elseif ($sentAuthCode !== $user->getEmailAuthCode()) {
-            $event->setUser(new NullUser());
-        }
 
         return $event;
     }
