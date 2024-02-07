c5b247b67bcd1faf23d763a7980fb69c1cc19f25
NilsRec <nils.lapotre@reconnect.fr>
2024-02-07 15:28:36 +0100

    :sparkles: [2FA] Can now choose 2FA method

---
+++ src/Migrations/Version20240207132126.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240207132126 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user ADD mfaMethod VARCHAR(255) DEFAULT "email"');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user DROP mfaMethod');
+    }
+}
