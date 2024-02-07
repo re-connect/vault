1c1a9837e99f029eb2015f2d93461907583b4929
NilsRec <nils.lapotre@reconnect.fr>
2024-02-07 17:08:30 +0100

    :sparkles: [2FA] Add validation constraint on 2FA

--- src/ControllerV2/UserController.php
+++ src/ControllerV2/UserController.php
@@ -74,13 +74,17 @@ class UserController extends AbstractController
             'checkCurrentPassword' => true,
         ])->handleRequest($request);
 
-        if ($userForm->isSubmitted() && $userForm->isValid()) {
-            $usernameWillBeUpdated = $userManager->getUniqueUsername($user) !== $user->getUsername();
-            $em->flush();
-            $message = sprintf('settings_saved_successfully%s', $usernameWillBeUpdated ? '_username_updated' : '');
-            $this->addFlash('success', $message);
+        if ($userForm->isSubmitted()) {
+            if ($userForm->isValid()) {
+                $usernameWillBeUpdated = $userManager->getUniqueUsername($user) !== $user->getUsername();
+                $em->flush();
+                $message = sprintf('settings_saved_successfully%s', $usernameWillBeUpdated ? '_username_updated' : '');
+                $this->addFlash('success', $message);
 
-            return $this->redirectToRoute('user_settings');
+                return $this->redirectToRoute('user_settings');
+            }
+
+            $this->addFlash('error', 'form_contains_error');
         }
 
         if ($passwordForm->isSubmitted()) {
