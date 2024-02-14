347b8da92cd43d70d75c1837a7866347d76078e0
NilsRec <nils.lapotre@reconnect.fr>
2024-02-14 15:27:11 +0100

    :recycle: [Fixtures] Refaco to prepare tests

--- src/DataFixtures/v2/BeneficiaryFixture.php
+++ src/DataFixtures/v2/BeneficiaryFixture.php
@@ -21,8 +21,6 @@ use Doctrine\Persistence\ObjectManager;
 
 class BeneficiaryFixture extends Fixture implements FixtureGroupInterface
 {
-    public const WEAK_PASSWORD_HASH = '$2y$13$1e.Kr4Ru31eHQBKU3d6BY..EIerE6/IYA5K/JxMjGwBYb5dL7B6eG'; // = 'password'
-    public const STRONG_PASSWORD_HASH = '$2y$13$te1UUDYPXELYC9jcVmil0.XQcmPValnWUN10VqDAJsh5zpnkiT9fm'; // = 'StrongPassword1!'
     public const BENEFICIARY_MAIL = 'v2_test_user_beneficiary@mail.com';
     public const BENEFICIARY_MAIL_SETTINGS = 'v2_test_user_beneficiary_settings@mail.com';
     public const BENEFICIARY_MAIL_SETTINGS_EDIT = 'v2_test_user_beneficiary_settings_edit@mail.com';
@@ -51,7 +49,7 @@ class BeneficiaryFixture extends Fixture implements FixtureGroupInterface
         $this->createTestBeneficiary($this->getTestUser(self::BENEFICIARY_MAIL_IN_CREATION), [], [], true);
         $this->createTestBeneficiary($this->getTestUser(self::BENEFICIARY_PASSWORD_WEAK)
             ->setHasPasswordWithLatestPolicy(false)
-            ->setPassword(self::WEAK_PASSWORD_HASH)
+            ->setPassword(UserFactory::WEAK_PASSWORD_HASH)
         );
 
         $manager->flush();
