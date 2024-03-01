d7b6e4cce566f1d297b2156d985b30101de18de3
NilsRec <nils.lapotre@reconnect.fr>
2024-03-01 17:56:16 +0100

    :recycle: [DuplicatedBenefMail] Refacto email class

--- src/ServiceV2/Mailer/Email/DuplicatedUsernameEmail.php
+++ src/ServiceV2/Mailer/Email/DuplicatedUsernameEmail.php
@@ -2,28 +2,9 @@
 
 namespace App\ServiceV2\Mailer\Email;
 
-use App\Entity\Centre;
-use App\Entity\Region;
-use App\Entity\User;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mime\Email;
-
-readonly class DuplicatedUsernameEmail
+class DuplicatedUsernameEmail extends LocalizedTemplatedEmail
 {
-    public static function create(string $defaultRecipient, User $duplicatedUser, User $authenticatedUser): Email
-    {
-        $pro = $authenticatedUser->getSubjectMembre();
-        $centres = $pro->getCentres();
-        $regions = $centres->map(fn (Centre $centre) => $centre->getRegion());
-        $recipients = $regions->map(fn (?Region $region) => $region?->getEmail())->filter(fn (?string $recipient) => null !== $recipient);
-        if ($recipients->isEmpty()) {
-            $recipients->add($defaultRecipient);
-        }
-
-        return (new TemplatedEmail())
-            ->to(...$recipients)
-            ->subject('duplicate_mail_subject')
-            ->htmlTemplate('v2/email/duplicated_user.html.twig')
-            ->context(['duplicatedUser' => $duplicatedUser, 'userLang' => 'fr', 'client' => $duplicatedUser->getCreatorClient(), 'centres' => $centres, 'pro' => $pro]);
-    }
+    protected const TEMPLATE_PATH = 'v2/email/duplicated_user.html.twig';
+    protected const SUBJECT = 'duplicate_mail_subject';
+    protected const TRANSLATION_ROUTE = null;
 }
