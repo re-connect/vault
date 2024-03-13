6bacf6691d2cd87639215c91b92d1c436521fa22
Antoine Aresu <aresu.antoine@gmail.com>
2024-03-13 13:41:30 +0100

    :recycle: :fire: [AbstractControllerV1] Refacto implementations with v2

--- src/Controller/Api/SwitchUserLocaleController.php
+++ src/Controller/Api/SwitchUserLocaleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Api;
 
-use App\Controller\AbstractController;
+use App\ControllerV2\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
