2bc75abee6bd7c0e90ef283d6ba509f80b65077e
AntoineAresu <aresu.antoine@gmail.com>
2024-03-15 10:52:32 +0100

    :sparkles: [BeneficiaireAdmin] Update exported fields

--- src/Admin/BeneficiaireAdmin.php
+++ src/Admin/BeneficiaireAdmin.php
@@ -329,8 +329,6 @@ class BeneficiaireAdmin extends AbstractAdmin
         return [
             'id' => 'id',
             "Nom d'utilisateur" => 'user.username',
-            'Nom' => 'user.nom',
-            'Prénom' => 'user.prenom',
             'Email' => 'user.email',
             'Télephone' => 'user.telephone',
             'Date de naissance' => 'dateNaissanceStr',
@@ -344,7 +342,10 @@ class BeneficiaireAdmin extends AbstractAdmin
             'Créé par (centre)' => 'user.creatorCentre',
             'Créé par (client)' => 'user.creatorClient',
             'Compte test' => 'user.testToString',
-            'Nombre de documents' => 'nbDocuments',
+            'Nombre de documents' => 'documentsCount',
+            "Nombre d'événements" => 'eventsCount',
+            'Nombre de contacts' => 'contactsCount',
+            'Nombre de notes' => 'notesCount',
             'Région' => 'getRegionToString',
         ];
     }
