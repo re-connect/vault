be38b118e194d89085b25d0367f45ecfb9469070
AntoineAresu <aresu.antoine@gmail.com>
2024-06-10 17:22:43 +0200

    :sparkles: [PersonalData] Anonymise name according to child entity

--- src/Domain/Anonymization/AnonymizationHelper.php
+++ src/Domain/Anonymization/AnonymizationHelper.php
@@ -4,6 +4,8 @@ namespace App\Domain\Anonymization;
 
 class AnonymizationHelper
 {
+    final public const string ANONYMIZED_SUBJECT = 'Titre anonymisé';
+    final public const string ANONYMIZED_DOCUMENT = 'Document anonymisé';
     final public const string ANONYMIZED_CONTENT = 'Ce contenu a été anonymisé pour protéger les données personnelles qui pourraient être présentes';
     final public const string ANONYMIZED_EMAIL = 'anonymized@yopmail.com';
 }
