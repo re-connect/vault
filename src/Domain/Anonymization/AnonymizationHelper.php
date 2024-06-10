47ab88e2301c93106cec5bb208fe24cb8dda70b3
AntoineAresu <aresu.antoine@gmail.com>
2024-06-10 15:16:44 +0200

    :sparkles: [Contact] Anonymize contacts

---
+++ src/Domain/Anonymization/AnonymizationHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Domain\Anonymization;
+
+class AnonymizationHelper
+{
+    final public const string ANONYMIZED_CONTENT = 'Ce contenu a été anonymisé pour protéger les données personnelles qui pourraient être présentes';
+    final public const string ANONYMIZED_EMAIL = 'anonymized@yopmail.com';
+}
