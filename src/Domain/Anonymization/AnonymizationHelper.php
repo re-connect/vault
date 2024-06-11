7b094f5cc18d9f7547b9cca5b8d5509455fcff7f
AntoineAresu <aresu.antoine@gmail.com>
2024-06-11 10:27:58 +0200

    :sparkles: [Document] Anonymize

--- src/Domain/Anonymization/AnonymizationHelper.php
+++ src/Domain/Anonymization/AnonymizationHelper.php
@@ -6,6 +6,9 @@ class AnonymizationHelper
 {
     final public const string ANONYMIZED_SUBJECT = 'Titre anonymisé';
     final public const string ANONYMIZED_DOCUMENT = 'Document anonymisé';
+    final public const string ANONYMIZED_DOCUMENT_EXTENSION = 'png';
+    final public const string ANONYMIZED_DOCUMENT_OBJECT_KEY = 'anonymous.png';
+    final public const string ANONYMIZED_DOCUMENT_THUMBNAIL_KEY = 'anonymous-thumbnail.png';
     final public const string ANONYMIZED_CONTENT = 'Ce contenu a été anonymisé pour protéger les données personnelles qui pourraient être présentes';
     final public const string ANONYMIZED_EMAIL = 'anonymized@yopmail.com';
 }
