cef28d0d1f5d13a019f4bf56318fec9c6c243391
AntoineAresu <aresu.antoine@gmail.com>
2024-06-21 14:17:51 +0200

    :bug: [derniereConnexionAt] Fix getter in order to run command properly

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -742,7 +742,7 @@ class User extends BaseUser implements \JsonSerializable, TwoFactorInterface, Tw
 
     public function getDerniereConnexionAt(): ?\DateTime
     {
-        return $this->lastLogin;
+        return $this->derniereConnexionAt;
     }
 
     public function getAvatar(): ?string
