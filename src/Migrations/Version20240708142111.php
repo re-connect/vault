c3cb3f388d0b3619ea7ff784956ea7343086e44a
NilsRec <nils.lapotre@reconnect.fr>
2024-07-08 16:59:32 +0200

    :card_file_box: [FeatureFlag] Added entity and admin page

---
+++ src/Migrations/Version20240708142111.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240708142111 extends AbstractMigration
+{
+    #[\Override]
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    #[\Override]
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE feature_flag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, enabled TINYINT(1) NOT NULL, enableDate DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    #[\Override]
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE feature_flag');
+    }
+}
