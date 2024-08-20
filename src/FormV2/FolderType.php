f6ccfa41bd89ea35fc51dba2fe990941a6134d7b
AntoineAresu <aresu.antoine@gmail.com>
2024-08-20 17:17:49 +0200

    :sparkles: [FolderIcon] Allow choose on update, display dynamically icon on select

--- src/FormV2/FolderType.php
+++ src/FormV2/FolderType.php
@@ -5,6 +5,7 @@ namespace App\FormV2;
 use App\Entity\Attributes\FolderIcon;
 use App\Entity\Dossier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -13,6 +14,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FolderType extends AbstractType
 {
+    public function __construct(private readonly Packages $packages)
+    {
+    }
+
     #[\Override]
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -21,6 +26,23 @@ class FolderType extends AbstractType
                 'label' => 'name',
                 'attr' => [
                     'data-action' => 'click->autocomplete#autocomplete',
+                ],
+            ])
+            ->add('icon', EntityType::class, [
+                'class' => FolderIcon::class,
+                'required' => false,
+                'label' => 'choose_picture',
+                'choice_label' => 'name',
+                'placeholder' => 'neutral',
+                'placeholder_attr' => [
+                    'data-icon-file-path' => $this->packages->getUrl(Dossier::DEFAULT_ICON_FILE_PATH),
+                ],
+                'choice_attr' => fn ($choice) => [
+                    'data-icon-file-path' => $this->packages->getUrl($choice->getPublicFilePath()),
+                ],
+                'attr' => [
+                    'data-folder-icon-target' => 'input',
+                    'data-action' => 'change->folder-icon#update',
                 ],
             ]);
 
@@ -35,13 +57,6 @@ class FolderType extends AbstractType
                         'shared' => false,
                     ],
                     'data' => $options['private'],
-                ])
-                ->add('icon', EntityType::class, [
-                    'class' => FolderIcon::class,
-                    'required' => false,
-                    'label' => 'icon',
-                    'choice_label' => 'name',
-                    'placeholder' => 'no_icon',
                 ]);
         }
     }
