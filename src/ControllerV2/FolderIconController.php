ad2e4a314995f7a1b5efa3dcb00e03effdc0c991
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-09-02 22:56:56 +0200

    :sparkles: [Folder icons] Create api route to get folder icons

--- src/ControllerV2/FolderIconController.php
+++ src/ControllerV2/FolderIconController.php
@@ -2,10 +2,13 @@
 
 namespace App\ControllerV2;
 
+use App\Entity\Attributes\FolderIcon;
 use App\Repository\FolderIconRepository;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FolderIconController extends AbstractController
 {
@@ -13,6 +16,15 @@ class FolderIconController extends AbstractController
     #[Route(path: '/folder_icon/{name}', name: 'folder_icon_display')]
     public function show(string $name, FolderIconRepository $repository): BinaryFileResponse
     {
-        return new BinaryFileResponse($repository->findOneBy(['name' => $name]));
+        return $this->file($repository->findOneBy(['name' => $name]));
+    }
+
+    #[Route(path: '/public/folder_icons', name: 'folder_icons')]
+    public function list(FolderIconRepository $repository, TranslatorInterface $translator): JsonResponse
+    {
+        return $this->json(array_map(fn (FolderIcon $folderIcon) => [
+            'name' => $translator->trans($folderIcon->getName()),
+            'url' => $folderIcon->getPublicFilePath(),
+        ], $repository->findAll()));
     }
 }
