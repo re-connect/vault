5ef9f6a2bea1709aed7d89c1de30a7622bca9527
Antoine Aresu <aresu.antoine@gmail.com>
2024-10-30 13:57:08 +0100

    :recycle: [yarn-security-checker] Refacto script to make it more readable

--- yarn-security-checker.sh
+++ yarn-security-checker.sh
@@ -3,13 +3,17 @@
 # Check the output of yarn audit, return exit code 1 if high or critical vulnerabilities found
 
 FAIL=0
+SEVERITIES=("Moderate" "High" "Critical")
 SEVERITY_SUMMARY=$(yarn audit | tail -3 | head -2)
 SEVERITY_COUNT=$(echo "$SEVERITY_SUMMARY" | tail -1)
-HIGH_CRITICAL_COUNT_ARRAY=$(echo "$SEVERITY_COUNT" | awk '{print $5, $8, $11}' | tr ";" "\n")
 
-for HIGH_CRITICAL_SEVERITY_COUNT in $HIGH_CRITICAL_COUNT_ARRAY; do
-  if [ "$HIGH_CRITICAL_SEVERITY_COUNT" -ne 0 ]; then
-    echo "$SEVERITY_SUMMARY"
-    exit 1
+for SEVERITY in "${SEVERITIES[@]}"; do
+  if echo "$SEVERITY_COUNT" | grep -q "$SEVERITY"; then
+    FAIL=1
   fi
 done
+
+if [ $FAIL -eq 1 ]; then
+  echo "$SEVERITY_SUMMARY"
+  exit 1
+fi
