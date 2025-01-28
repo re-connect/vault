2f1c98b4decace8095ad58697df26ae80a1c824d
AntoineAresu <aresu.antoine@gmail.com>
2025-01-28 11:37:39 +0100

    :recycle: [MaintenanceListener] Refacto api request check

--- src/ListenerV2/MaintenanceListener.php
+++ src/ListenerV2/MaintenanceListener.php
@@ -9,13 +9,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
 #[AsEventListener(event: KernelEvents::REQUEST, method: 'onKernelRequest', priority: 500)]
 readonly class MaintenanceListener
 {
-    public function __construct(private Environment $environment, private FeatureFlagChecker $featureFlagChecker, private TranslatorInterface $translator)
+    public function __construct(private Environment $environment, private FeatureFlagChecker $featureFlagChecker)
     {
     }
 
@@ -25,8 +24,13 @@ readonly class MaintenanceListener
             return;
         }
 
-        str_starts_with($event->getRequest()->getPathInfo(), '/api')
-            ? $event->setResponse(new JsonResponse(['message' => $this->translator->trans('maintenance')], Response::HTTP_SERVICE_UNAVAILABLE))
+        $this->isApiRequest($event->getRequest()->getPathInfo())
+            ? $event->setResponse(new JsonResponse(['error' => 'The service is temporarily unavailable.'], Response::HTTP_SERVICE_UNAVAILABLE))
             : $event->setResponse(new Response($this->environment->render('v2/maintenance/maintenance.html.twig')));
+    }
+
+    public function isApiRequest(string $requestPathInfo): bool
+    {
+        return str_starts_with($requestPathInfo, '/api') || str_starts_with($requestPathInfo, '/oauth');
     }
 }
