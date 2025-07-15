20157da976a40894f9bd60714164e7c4d1f7ece9
Antoine Aresu <aresu.antoine@gmail.com>
2025-07-15 09:16:09 +0200

    :wrench: [deploy] Reduce keeped releases count

--- deploy.php
+++ deploy.php
@@ -34,6 +34,7 @@ add('writable_dirs', []);
 host('vault-pp')
     ->setLabels(['stage' => 'preprod'])
     ->set('branch', 'dev')
+    ->set('keep_releases', 4)
     ->set('deploy_path', '~/www')
     ->set('http_user', 'preprod_reconnect_fr')
     ->set('homepage_url', 'https://preprod.reconnect.fr')
