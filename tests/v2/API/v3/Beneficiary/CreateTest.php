79d4bdf4a1ffa348acfd2512c8af8d577ef1daa6
NilsReco <nils.lapotre@reconnect.fr>
2025-07-18 15:33:14 +0200

    ✅ [Beneficiary] Updated CreateTest

--- tests/v2/API/v3/Beneficiary/CreateTest.php
+++ tests/v2/API/v3/Beneficiary/CreateTest.php
@@ -15,10 +15,27 @@ class CreateTest extends AbstractApiTest
         parent::setUp();
     }
 
-    public function testCreateBeneficiary(): void
+    /**
+     * @dataProvider canCreateProvider
+     */
+    public function testCreateBeneficiary(string $clientName, ?string $externalCenterId = null): void
     {
+        $beneficiary = [
+            'last_name' => 'test',
+            'first_name' => 'api',
+            'birth_date' => '2023-02-13T13:44:28.762Z',
+            'email' => 'api@test.com',
+            'phone' => '1234567890',
+            'distant_id' => '1200',
+            'external_pro_id' => '4972',
+        ];
+
+        if ($externalCenterId) {
+            $beneficiary['external_center'] = $externalCenterId;
+        }
+
         $this->assertEndpoint(
-            'reconnect_pro',
+            $clientName,
             '/beneficiaries',
             'POST',
             201,
@@ -40,6 +57,38 @@ class CreateTest extends AbstractApiTest
                     'b_first_mobile_connexion' => false,
                 ],
             ],
+            $beneficiary
+        );
+
+        $benef = $this->repo->findByUsername('api.test.13/02/2023');
+        $this->assertNotNull($benef);
+        $this->assertNotEmpty($benef->getExternalLinks());
+        $externalLink = $benef->getExternalLinks()->first();
+        $this->assertEquals($clientName, $externalLink->getClient()->getNom());
+        if ($externalCenterId) {
+            $beneficiaireCentre = $benef->getBeneficiairesCentres()->first();
+            $this->assertEquals($beneficiaireCentre, $externalLink->getBeneficiaireCentre());
+            $this->assertTrue($beneficiaireCentre->getBValid());
+        }
+        $this->assertEquals(1200, $benef->getExternalLinks()->first()->getDistantId());
+    }
+
+    /**
+     * @dataProvider canNotCreateProvider
+     */
+    public function testCanNotCreateBeneficiary(string $clientName): void
+    {
+        $this->assertEndpoint(
+            $clientName,
+            '/beneficiaries',
+            'POST',
+            403,
+            [
+                '@context' => '/api/contexts/Error',
+                '@type' => 'hydra:Error',
+                'hydra:title' => 'An error occurred',
+                'hydra:description' => 'Access Denied.',
+            ],
             [
                 'last_name' => 'test',
                 'first_name' => 'api',
@@ -47,18 +96,21 @@ class CreateTest extends AbstractApiTest
                 'email' => 'api@test.com',
                 'phone' => '1234567890',
                 'distant_id' => '1200',
-                'external_center' => '42',
-                'external_pro_id' => '4972',
             ]
         );
-        $benef = $this->repo->findByUsername('api.test.13/02/2023');
-        $this->assertNotNull($benef);
-        $this->assertNotEmpty($benef->getExternalLinks());
-        $externalLink = $benef->getExternalLinks()->first();
-        $beneficiaireCentre = $benef->getBeneficiairesCentres()->first();
-        $this->assertEquals('reconnect_pro', $externalLink->getClient()->getNom());
-        $this->assertEquals($beneficiaireCentre, $externalLink->getBeneficiaireCentre());
-        $this->assertEquals(1200, $benef->getExternalLinks()->first()->getDistantId());
-        $this->assertTrue($beneficiaireCentre->getBValid());
+    }
+
+    public function canNotCreateProvider(): \Generator
+    {
+        yield 'Should not create beneficiary for client with readonly scopes' => ['read_only'];
+        yield 'Should not create beneficiary for client with no scopes' => ['no_scopes'];
+    }
+
+    public function canCreateProvider(): \Generator
+    {
+        yield 'Should create beneficiary for client with update scopes' => ['read_and_update'];
+        yield 'Should create beneficiary for client with create scopes' => ['create_only'];
+        yield 'Should create beneficiary with external center for Reconnect Pro client' => ['reconnect_pro', '42'];
+        yield 'Should create beneficiary for Rosalie client ' => ['rosalie'];
     }
 }
