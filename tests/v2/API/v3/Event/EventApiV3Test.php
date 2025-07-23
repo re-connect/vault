51b87389c07ed5f2e910e8a085b58b86abf12b73
NilsReco <nils.lapotre@reconnect.fr>
2025-07-23 10:29:37 +0200

    ✅ [Event] Updated api tests

--- tests/v2/API/v3/Event/EventApiV3Test.php
+++ tests/v2/API/v3/Event/EventApiV3Test.php
@@ -3,7 +3,6 @@
 namespace App\Tests\v2\API\v3\Event;
 
 use App\DataFixtures\v2\BeneficiaryFixture;
-use App\Entity\Attributes\Beneficiaire;
 use App\Tests\Factory\BeneficiaireFactory;
 use App\Tests\Factory\ClientFactory;
 use App\Tests\Factory\EventFactory;
@@ -11,13 +10,53 @@ use App\Tests\v2\API\v3\AbstractApiTest;
 
 class EventApiV3Test extends AbstractApiTest
 {
-    public function testGetCollection(): void
+    /**
+     * @dataProvider canGetProvider
+     */
+    public function testGetCollection(string $clientName): void
     {
-        $client = ClientFactory::find(['nom' => 'reconnect_pro'])->object();
-        /** @var Beneficiaire $beneficiary */
-        $beneficiary = $this->beneficiaireRepository->findByClientIdentifier($client->getRandomId())[0];
+        $client = ClientFactory::find(['nom' => $clientName])->object();
+        $beneficiaries = $this->beneficiaireRepository->findByClientIdentifier($client->getRandomId());
+        $eventCounts = 0;
+        foreach ($beneficiaries as $beneficiary) {
+            $eventCounts += $beneficiary->getEvenements()->filter(function ($event) {
+                return !$event->getBPrive();
+            })->count();
+        }
+
         $this->assertEndpoint(
-            'reconnect_pro',
+            $clientName,
+            '/events',
+            'GET',
+            200,
+            [
+                '@context' => '/api/contexts/Event',
+                '@type' => 'hydra:Collection',
+                'hydra:totalItems' => $eventCounts,
+            ]
+        );
+    }
+
+    /**
+     * @dataProvider canNotGetProvider
+     */
+    public function testCanNotGetCollection(string $clientName): void
+    {
+        $this->assertEndpointAccessIsDenied(
+            $clientName,
+            '/events',
+            'GET',
+        );
+    }
+
+    /**
+     * @dataProvider canGetProvider
+     */
+    public function testGetCollectionFromBeneficiary(string $clientName): void
+    {
+        $beneficiary = $this->getBeneficiaryForClient($clientName);
+        $this->assertEndpoint(
+            $clientName,
             sprintf('/beneficiaries/%s/events', $beneficiary->getId()),
             'GET',
             200,
@@ -30,15 +69,32 @@ class EventApiV3Test extends AbstractApiTest
         );
     }
 
-    public function testGetOne(): void
+    /**
+     * @dataProvider canNotGetProvider
+     */
+    public function testCanNotGetCollectionFromBeneficiary(string $clientName): void
     {
-        $this->markTestSkipped('Event api ressource is currently disabled');
+        $beneficiary = $this->getBeneficiaryForClient($clientName);
+        $this->assertEndpointAccessIsDenied(
+            $clientName,
+            sprintf('/beneficiaries/%s/events', $beneficiary->getId()),
+            'GET',
+        );
+    }
+
+    /**
+     * @dataProvider canGetProvider
+     */
+    public function testGetOne(string $clientName): void
+    {
+        $beneficiary = $this->getBeneficiaryForClient($clientName);
         $event = EventFactory::findOrCreate([
-            'beneficiaire' => BeneficiaireFactory::findByEmail(BeneficiaryFixture::BENEFICIARY_MAIL),
+            'beneficiaire' => $beneficiary,
+            'bPrive' => false,
         ])->object();
 
         $this->assertEndpoint(
-            'rosalie',
+            $clientName,
             sprintf('/events/%d', $event->getId()),
             'GET',
             200,
@@ -55,10 +111,45 @@ class EventApiV3Test extends AbstractApiTest
         );
     }
 
-    public function testPost(): void
+    /**
+     * @dataProvider canNotGetProvider
+     */
+    public function testCanNotGetOne(string $clientName): void
+    {
+        $beneficiary = $this->getBeneficiaryForClient($clientName);
+        $event = EventFactory::findOrCreate([
+            'beneficiaire' => $beneficiary,
+            'bPrive' => false,
+        ])->object();
+
+        $this->assertEndpointAccessIsDenied(
+            $clientName,
+            sprintf('/events/%d', $event->getId()),
+            'GET',
+        );
+    }
+
+    public function canGetProvider(): \Generator
+    {
+        yield 'Should read when read and update scopes' => ['read_and_update'];
+        yield 'Should read with Reconnect Pro client' => ['reconnect_pro'];
+        yield 'Should read with Rosalie client ' => ['rosalie'];
+        yield 'Should read with read only scopes' => ['read_only'];
+    }
+
+    public function canNotGetProvider(): \Generator
+    {
+        yield 'Should not read with create only scopes' => ['create_only'];
+        yield 'Should not read with no scopes' => ['no_scopes'];
+    }
+
+    /**
+     * @dataProvider canCreateProvider
+     */
+    public function testPost(string $clientName): void
     {
         $event = [
-            'beneficiaire_id' => BeneficiaireFactory::findByEmail(BeneficiaryFixture::BENEFICIARY_WITH_RP_LINK)->object()->getId(),
+            'beneficiaire_id' => $this->getBeneficiaryForClient($clientName)->getId(),
             'nom' => 'testNom',
             'date' => (new \DateTime('tomorrow'))->format('c'),
             'lieu' => 'testLieu',
@@ -66,7 +157,7 @@ class EventApiV3Test extends AbstractApiTest
         ];
 
         $this->assertEndpoint(
-            'reconnect_pro',
+            $clientName,
             '/events',
             'POST',
             201,
@@ -77,6 +168,110 @@ class EventApiV3Test extends AbstractApiTest
             ],
             $event
         );
+    }
+
+    /**
+     * @dataProvider canNotCreateProvider
+     */
+    public function testCanNotPost(string $clientName): void
+    {
+        $event = [
+            'beneficiaire_id' => $this->getBeneficiaryForClient($clientName)->getId(),
+            'nom' => 'testNom',
+            'date' => (new \DateTime('tomorrow'))->format('c'),
+            'lieu' => 'testLieu',
+            'b_prive' => false,
+        ];
+
+        $this->assertEndpointAccessIsDenied(
+            $clientName,
+            '/events',
+            'POST',
+            $event
+        );
+    }
+
+    public function canCreateProvider(): \Generator
+    {
+        yield 'Should create with only create scopes' => ['create_only'];
+        yield 'Should create with RP scopes' => ['reconnect_pro'];
+        yield 'Should create with Rosalie scopes' => ['rosalie'];
+    }
+
+    public function canNotCreateProvider(): \Generator
+    {
+        yield 'Should not create with only read scopes' => ['read_only'];
+        yield 'Should not create with no scopes' => ['no_scopes'];
+    }
+
+    /**
+     * @dataProvider canUpdateProvider
+     */
+    public function testToggleVisibility(string $clientName): void
+    {
+        $beneficiary = $this->getBeneficiaryForClient($clientName);
+        $event = EventFactory::findOrCreate([
+            'beneficiaire' => $beneficiary,
+            'bPrive' => false,
+        ])->object();
+        $eventId = $event->getId();
+
+        $this->assertEndpoint(
+            $clientName,
+            sprintf('/events/%s/toggle-visibility', $eventId),
+            'PATCH',
+            200,
+            [
+                '@context' => '/api/contexts/Event',
+                '@id' => sprintf('/api/v3/events/%s/toggle-visibility', $eventId),
+                '@type' => 'Event',
+            ],
+            []
+        );
+
+        // Once item has been set to private, it should not be found
+        $this->assertEndpoint(
+            $clientName,
+            sprintf('/events/%s/toggle-visibility', $eventId),
+            'PATCH',
+            404,
+            null,
+            []
+        );
+    }
+
+    /**
+     * @dataProvider canNotUpdateProvider
+     */
+    public function testCanNotToggleVisibility(string $clientName): void
+    {
+        $beneficiary = $this->getBeneficiaryForClient($clientName);
+        $event = EventFactory::findOrCreate([
+            'beneficiaire' => $beneficiary,
+            'bPrive' => false,
+        ])->object();
+        $eventId = $event->getId();
+
+        $this->assertEndpointAccessIsDenied(
+            $clientName,
+            sprintf('/events/%s/toggle-visibility', $eventId),
+            'PATCH',
+            []
+        );
+    }
+
+    public function canUpdateProvider(): \Generator
+    {
+        yield 'Should update with read_and_update scopes' => ['read_and_update'];
+        yield 'Should update with Reconnect Pro scopes' => ['reconnect_pro'];
+    }
+
+    public function canNotUpdateProvider(): \Generator
+    {
+        yield 'Should not update with read only scopes' => ['read_only'];
+        yield 'Should not update with Rosalie scopes' => ['rosalie'];
+        yield 'Should not update with no scopes' => ['no_scopes'];
+        yield 'Should not update with create only scopes' => ['create_only'];
     }
 
     public function testPut(): void
@@ -151,40 +346,6 @@ class EventApiV3Test extends AbstractApiTest
             sprintf('/events/%d', $event->getId()),
             'DELETE',
             204,
-        );
-    }
-
-    public function testToggleVisibility(): void
-    {
-        $client = ClientFactory::find(['nom' => 'reconnect_pro'])->object();
-        /** @var Beneficiaire $beneficiary */
-        $beneficiary = $this->beneficiaireRepository->findByClientIdentifier($client->getRandomId())[0];
-        $event = EventFactory::findOrCreate([
-            'beneficiaire' => $beneficiary,
-            'bPrive' => false,
-        ])->object();
-        $eventId = $event->getId();
-
-        $this->assertEndpoint(
-            'reconnect_pro',
-            sprintf('/events/%s/toggle-visibility', $eventId),
-            'PATCH',
-            200,
-            [
-                '@context' => '/api/contexts/Event',
-                '@id' => sprintf('/api/v3/events/%s/toggle-visibility', $eventId),
-                '@type' => 'Event',
-            ],
-            []
-        );
-        // Once item has been set to private, it should not be found
-        $this->assertEndpoint(
-            'reconnect_pro',
-            sprintf('/events/%s/toggle-visibility', $eventId),
-            'PATCH',
-            404,
-            null,
-            []
         );
     }
 }
