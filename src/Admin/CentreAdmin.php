4ff8b9219e38fb00ed7576ad411308567b3f101f
Antoine Aresu <aresu.antoine@gmail.com>
2025-08-20 17:00:55 +0200

    :sparkles: [CentreAdmin] Add client creator on list, edit and filters

--- src/Admin/CentreAdmin.php
+++ src/Admin/CentreAdmin.php
@@ -4,7 +4,9 @@ namespace App\Admin;
 
 use App\Entity\Attributes\Association;
 use App\Entity\Attributes\Centre;
+use App\Entity\Attributes\Client;
 use App\Entity\Attributes\CreatorCentre;
+use App\Entity\Attributes\CreatorClient;
 use App\Entity\Attributes\Region;
 use App\EventSubscriber\AssociationCreationSubscriber;
 use App\ManagerV2\UserManager;
@@ -13,8 +15,11 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridInterface;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Filter\Model\FilterData;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\AdminType;
+use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\DoctrineORMAdminBundle\Filter\CallbackFilter;
 use Sonata\Form\Type\CollectionType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -82,6 +87,9 @@ class CentreAdmin extends AbstractAdmin
                 'disabled' => true,
             ])
             ->add('nom')
+            ->add('creatorClient', null, [
+                'label' => 'Créé par (client)',
+                'attr' => ['read_only' => true], 'disabled' => true])
             ->add('region', EntityType::class, [
                 'class' => Region::class,
                 'required' => false,
@@ -140,6 +148,25 @@ class CentreAdmin extends AbstractAdmin
             ->add('id')
             ->add('nom')
             ->add('region', null, ['label' => 'Région'])
+            ->add('creatorClient', CallbackFilter::class, [
+                'label' => 'Créé par (client)',
+                'callback' => static function (ProxyQueryInterface $query, string $alias, string $field, FilterData $data): bool {
+                    if (!$data->hasValue()) {
+                        return false;
+                    }
+                    $value = $data->getValue();
+
+                    $query
+                        ->join(CreatorClient::class, 'creator_client', 'WITH', $alias.'.id = creator_client.centre')
+                        ->join('creator_client.entity', 'client')
+                        ->where('client IN (:value)')
+                        ->setParameter('value', $value);
+
+                    return true;
+                },
+                'field_type' => EntityType::class,
+                'field_options' => ['class' => Client::class, 'multiple' => true],
+            ])
             ->add('createdAt', null, ['label' => 'Date de création'])
             ->add('test', null, [
                 'label' => 'Compte test',
@@ -155,6 +182,7 @@ class CentreAdmin extends AbstractAdmin
             ->addIdentifier('id', null, ['route' => ['name' => 'edit']])
             ->addIdentifier('nom', null, ['route' => ['name' => 'edit']])
             ->add('association', null, ['label' => 'Association'])
+            ->add('creatorClient', null, ['label' => 'Créé par (client)'])
             ->add('createdAt', null, ['label' => 'Date de création'])
             ->add('canada', null, ['label' => 'Canada'])
             ->add('_action', 'actions', [
