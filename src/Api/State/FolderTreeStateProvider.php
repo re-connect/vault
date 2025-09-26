4f0e7b3b65007edc7431fd48bff688b29e63bcc0
NilsReco <nils.lapotre@reconnect.fr>
2025-09-26 10:08:04 +0200

    🐛 [Folders] Fix missing filter on external link

--- src/Api/State/FolderTreeStateProvider.php
+++ src/Api/State/FolderTreeStateProvider.php
@@ -4,12 +4,14 @@ namespace App\Api\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\Api\Manager\ApiClientManager;
+use App\Entity\Attributes\Beneficiaire;
 use App\Entity\Attributes\Dossier;
 use Doctrine\ORM\EntityManagerInterface;
 
 class FolderTreeStateProvider implements ProviderInterface
 {
-    public function __construct(private readonly EntityManagerInterface $em)
+    public function __construct(private readonly EntityManagerInterface $em, private readonly ApiClientManager $apiClientManager)
     {
     }
 
@@ -17,7 +19,12 @@ class FolderTreeStateProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
         $repo = $this->em->getRepository(Dossier::class);
+        $benef = $this->em->getRepository(Beneficiaire::class)->find($uriVariables['id']);
+        $client = $this->apiClientManager->getCurrentOldClient();
+        if (!$benef || ($client && !$benef->hasExternalLinkForClient($client))) {
+            return null;
+        }
 
-        return $repo->findBy(['dossierParent' => null, 'beneficiaire' => $uriVariables['id'], 'bPrive' => false]);
+        return $repo->findBy(['dossierParent' => null, 'beneficiaire' => $benef, 'bPrive' => false]);
     }
 }
