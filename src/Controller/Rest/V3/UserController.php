60f7fb0fe2bca9cdc7e3a8083188fe91b32ed89d
NilsReco <nils.lapotre@reconnect.fr>
2026-02-19 08:20:02 +0100

    🐛 [User] Fix request personal data endpoint

--- src/Controller/Rest/V3/UserController.php
+++ src/Controller/Rest/V3/UserController.php
@@ -5,14 +5,15 @@ namespace App\Controller\Rest\V3;
 use App\ControllerV2\AbstractController;
 use App\Entity\User;
 use App\ServiceV2\Mailer\MailerService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(path: '/api/v3/users', name: 'api_user_')]
+#[Route(path: '/api/v3/user', name: 'api_user_')]
 class UserController extends AbstractController
 {
     #[Route(path: '/request-personal-account-data', name: 'request_personal_account_data', methods: ['GET'])]
-    public function requestPersonalAccountData(MailerService $mailer): Response
+    public function requestPersonalAccountData(MailerService $mailer, EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
 
@@ -22,8 +23,10 @@ class UserController extends AbstractController
 
         if (!$user->hasRequestedPersonalAccountData()) {
             $mailer->sendPersonalDataRequestEmail($user);
+            $user->setPersonalAccountDataRequestedAt(new \DateTimeImmutable());
+            $em->flush();
         }
 
-        return $this->json($user);
+        return $this->json($user, Response::HTTP_CREATED, [], ['groups' => 'user:read']);
     }
 }
