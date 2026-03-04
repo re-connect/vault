bf5f621e92cd9342982e366ee2b9f5fb55d77908
Romain Reconnect <romain.lacits@reconnect.fr>
2026-03-04 15:20:53 +0100

    🚚 [Test] update URL in DigitalLabsPageTest

--- tests/v2/Controller/MainController/DigitalLabsPageTest.php
+++ tests/v2/Controller/MainController/DigitalLabsPageTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\v2\Controller\MainController;
 
 class DigitalLabsPageTest extends AbstractPageTest
 {
-    protected const URL = '/reconnect-accompagnement-numerique';
+    protected const URL = '/nos-ateliers-numeriques';
     protected const PAGE_TITLE = 'innovative_solution_digital_divide';
     protected const SELECTOR = 'h3';
 }
