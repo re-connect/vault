7471c1d0c355c9e3deefaac2a48dc1a0aa3b1ce3
NilsReco <nils.lapotre@reconnect.fr>
2026-04-28 14:56:25 +0200

    🐛 [Event] Fix date timezone

--- src/ControllerV2/BeneficiaryPersonalDataController.php
+++ src/ControllerV2/BeneficiaryPersonalDataController.php
@@ -206,7 +206,7 @@ class BeneficiaryPersonalDataController extends AbstractController
         )->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $event->setDate($form->get('date')->getData());
+            $event->setDate($form->get('date')->getData()->setTimezone(new \DateTimeZone($event->getTimezone())));
             $em->persist($event);
             $em->flush();
             $this->addFlash('success', 'event_created');
